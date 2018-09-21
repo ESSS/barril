13412694f40e88ceb9a61c91ed5423b6a3110443
Bruno Oliveira <bruno@esss.com.br>
2018-09-21 12:39:39 -0300

    Drop py35 and add py37

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
