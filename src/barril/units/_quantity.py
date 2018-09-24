e10cc61f48c525fddc17d11c3ee941c107693d38
Bruno Oliveira <bruno@esss.com.br>
2018-09-24 11:00:12 -0300

    Do not check for isinstance in ObtainQuantity

--- src/barril/units/_quantity.py
+++ src/barril/units/_quantity.py
@@ -4,8 +4,6 @@ This module provides the implementation of an Quantity object.
 # NoQuantityReplacement
 from __future__ import absolute_import, unicode_literals
 
-import collections
-
 import six
 from six.moves import range, zip  # @UnresolvedImport
 
@@ -64,7 +62,7 @@ def ObtainQuantity(unit, category=None, unknown_unit_caption=None):
             unit = odict((cat, unit_and_exp) for (cat, unit_and_exp) in zip(category, unit))
             category = None
 
-    if isinstance(unit, collections.Mapping):
+    if hasattr(unit, 'items'):
         assert category is None
         if len(unit) == 1 and next(six.itervalues(unit))[1] == 1:
             # Although passed as composing, it's a simple case
@@ -209,7 +207,7 @@ class Quantity(object):
         unit_database = UnitDatabase.GetSingleton()
 
         if validate_category_and_units:
-            assert isinstance(category_to_unit_and_exps, collections.Mapping)
+            assert hasattr(category_to_unit_and_exps, 'items'), 'validate_category_and_units needs to be a dict'
             for category, (unit, _exp) in six.iteritems(category_to_unit_and_exps):
                 # will do the checkings needed for validation (category/unit)
                 # so that we can later just store the internal information without
