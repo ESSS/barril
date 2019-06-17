e6b6bb6de0f2c95a17f6868fa8e89983c0d4bd4c
icandeia <icandeia@esss.co>
2019-06-17 08:59:37 -0300

    Move unit system to barril.
    PWDA-5237

---
+++ src/barril/units/_tests/test_unit_system.py
@@ -0,0 +1,87 @@
+import pytest
+
+from barril.units.unit_system import UnitSystem
+
+
+@pytest.fixture
+def units_mapping_1():
+    units_mapping = {'length' : 'm', 'viscosity' : 'cP'}
+    return units_mapping
+
+
+@pytest.fixture
+def units_mapping_2():
+    units_mapping = {'length' : 'km', 'viscosity' : 'cP'}
+    return units_mapping
+
+
+def testUnitSystemManager(units_mapping_1):
+    system1 = UnitSystem('system1', 'My System', units_mapping_1)
+    system1.SetReadOnly(True)
+    assert system1.GetId() == 'system1'
+    assert system1.GetCaption() == 'My System'
+    assert system1.IsReadOnly() == True
+    assert system1.GetDefaultUnit('length') == 'm'
+    assert system1.GetDefaultUnit('viscosity') == 'cP'
+    assert system1.GetDefaultUnit('pressure') == None
+    assert system1.GetDefaultUnit('') == None
+
+
+def testEquality(units_mapping_1, units_mapping_2):
+    system1 = UnitSystem('system1', 'My System', units_mapping_1, True)
+    system2 = UnitSystem('system1', 'My System', units_mapping_1, True)
+    system3 = UnitSystem('system3', 'My System 3', units_mapping_2, False)
+
+    assert system1 == system2
+    assert not system1 != system2
+
+    assert system2 != system3
+    assert not system1 == system3
+
+    assert system1 != None
+    assert not system1 == None
+
+
+def testSetDefaultUnitCallback(units_mapping_1):
+    '''
+        Implements on_default_unit on unit system, called when the default unit changes.
+    '''
+
+    class MockClass:
+        callback_params = None
+
+        def OnDefaultUnit(self, category, unit):
+            self.callback_params = (category, unit)
+
+    obj = MockClass()
+    system1 = UnitSystem('system1', 'My System', units_mapping_1)
+    system1.on_default_unit.Register(obj.OnDefaultUnit)
+
+    system1.SetDefaultUnit('length', 'km')
+    assert obj.callback_params == ('length', 'km')
+
+    system1.SetDefaultUnit('viscosity', 'P')
+    assert obj.callback_params == ('viscosity', 'P')
+
+
+def testRemoveCategory(units_mapping_1):
+
+    class MockClass:
+        update_notified = None
+
+        def CallbackUnit(self, category, new_unit):
+            if category == 'length':
+                self.update_notified = True
+
+    system_1 = UnitSystem('system1', 'My System', units_mapping_1)
+
+    obj = MockClass()
+    system_1.on_default_unit.Register(obj.CallbackUnit)
+    assert system_1.GetDefaultUnit('length') == 'm'
+
+    # Removing the given quantity
+    obj.update_notified = False
+    system_1.RemoveCategory('length')
+    assert system_1.GetDefaultUnit('length') == None
+
+    assert obj.update_notified
---
+++ src/barril/units/unit_system.py
@@ -0,0 +1,83 @@
+from oop_ext.foundation import callback
+from oop_ext.foundation.decorators import Implements
+from oop_ext.interface import ImplementsInterface, IsImplementation
+from .unit_system_interface import IUnitSystem
+
+
+@ImplementsInterface(IUnitSystem)
+class UnitSystem:
+    '''
+    Default implementation for a Unit System.
+    .. see:: IUnitSystem
+    '''
+
+    @Implements(IUnitSystem.__init__)
+    def __init__(self, id, caption, units_mapping, read_only=False):
+        self._id = id
+        self._caption = caption
+        self._units_mapping = units_mapping
+        self._read_only = read_only
+        self.on_default_unit = callback.Callback()
+
+    @Implements(IUnitSystem.GetId)
+    def GetId(self):
+        return self._id
+
+    @Implements(IUnitSystem.GetCaption)
+    def GetCaption(self):
+        return self._caption
+
+    @Implements(IUnitSystem.SetCaption)
+    def SetCaption(self, caption):
+        self._caption = caption
+
+    @Implements(IUnitSystem.GetUnitsMapping)
+    def GetUnitsMapping(self):
+        return self._units_mapping
+
+    @Implements(IUnitSystem.IsReadOnly)
+    def IsReadOnly(self):
+        return self._read_only
+
+    @Implements(IUnitSystem.SetReadOnly)
+    def SetReadOnly(self, read_only):
+        self._read_only = read_only
+
+    # : duplicated signature to comply with Get/Set standard
+    GetReadOnly = IsReadOnly
+    read_only = property(GetReadOnly, SetReadOnly)
+
+    @Implements(IUnitSystem.GetDefaultUnit)
+    def GetDefaultUnit(self, category):
+        if not category:
+            return None
+
+        return self._units_mapping.get(category, None)
+
+    @Implements(IUnitSystem.RemoveCategory)
+    def RemoveCategory(self, category):
+        try:
+            del self._units_mapping[category]
+            self.on_default_unit(category, None)
+        except KeyError:
+            # The category is not in the unit system, so there is nothing to do
+            pass
+
+    @Implements(IUnitSystem.SetDefaultUnit)
+    def SetDefaultUnit(self, category, unit):
+        self._units_mapping[category] = unit
+        self.on_default_unit(category, unit)
+
+    @Implements(IUnitSystem.__eq__)
+    def __eq__(self, other):
+        if not IsImplementation(other, IUnitSystem):
+            return False
+
+        return self.GetId() == other.GetId() and \
+            self.GetCaption() == other.GetCaption() and \
+            self.GetUnitsMapping() == other.GetUnitsMapping() and \
+            self.IsReadOnly() == other.IsReadOnly()
+
+    @Implements(IUnitSystem.__ne__)
+    def __ne__(self, other):
+        return not self == other
